f9d1ac911b25b430f49df9e31b3442e1b685b6df
Angelo Zambrano <angelozam17@gmail>
2020-10-25 17:33:34 -0500

    test: enzyme introduction and setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
+Enzyme.configure({ adapter: new Adapter() })
+
+test('renders learn react link', () => {})
