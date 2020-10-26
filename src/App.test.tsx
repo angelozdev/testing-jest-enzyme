8180abef3240ee087bec2297bccda314227b0d15
Angelo Zambrano <angelozam17@gmail>
2020-10-25 20:34:31 -0500

    test: initial counter display

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,28 +1,50 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, ShallowWrapper } from 'enzyme'
 import App from './App'
 
+/** Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowWrapper}
+ **/
+const setup = (): ShallowWrapper => shallow(<App />)
+
+/** Find a react component by data-test attribute
+ * @function findByTestAttribute
+ * @returns {ShallowWrapper}
+ **/
+const findByTestAttribute = (
+  wrapper: ShallowWrapper,
+  value: string
+): ShallowWrapper => {
+  return wrapper.find(`[data-test='${value}']`)
+}
+
 test('renders without crashing', () => {
-  const wrapper = shallow(<App />)
-  const appComponent = wrapper.find('[data-test="component-app"]')
+  const wrapper = setup()
+  const appComponent = findByTestAttribute(wrapper, 'component-app')
 
   expect(appComponent).toHaveLength(1)
 })
 
 test('renders button', () => {
-  const wrapper = shallow(<App />)
-  const counterDisplay = wrapper.find('[data-test="counter-display"]')
+  const wrapper = setup()
+  const counterDisplay = findByTestAttribute(wrapper, 'counter-display')
 
   expect(counterDisplay).toHaveLength(1)
 })
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App />)
-  const incrementButton = wrapper.find('[data-test="increment-button"]')
+  const wrapper = setup()
+  const incrementButton = findByTestAttribute(wrapper, 'increment-button')
 
   expect(incrementButton).toHaveLength(1)
 })
 
-test('counter starts at 0', () => {})
+test('counter starts at 0', () => {
+  const wrapper = setup()
+  const counter = findByTestAttribute(wrapper, 'counter')
+
+  expect(counter.text()).toBe('0')
+})
 
 test('clicking on button increments counter display', () => {})
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css'
 
-function App() {
+const App = () => {
+  const [count, setCount] = React.useState(0)
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">This counter is current</h1>
+      <h1 data-test="counter-display">
+        This counter is currently
+        <span data-test="counter">{count}</span>
+      </h1>
       <button data-test="increment-button">Increment</button>
     </div>
   )
