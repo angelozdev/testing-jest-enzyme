c72b8c7fad6a518b7a85f84a16c77a945160dc0b
Angelo Zambrano <angelozam17@gmail>
2020-10-25 19:16:39 -0500

    build: install babel plugin react remove properties

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div data-test="component-app"></div>
+  return <div data-test="component-app">Hola mundo</div>
 }
 
 export default App
