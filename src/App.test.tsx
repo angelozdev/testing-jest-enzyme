05354302c0e5c663e349cff25fdbb9bf0a468515
Angelo Zambrano <angelozam17@gmail>
2020-10-25 19:46:25 -0500

    test: more data-test attributes tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,15 +4,24 @@ import App from './App'
 
 test('renders without crashing', () => {
   const wrapper = shallow(<App />)
-
   const appComponent = wrapper.find('[data-test="component-app"]')
 
   expect(appComponent).toHaveLength(1)
 })
 
-test('renders button', () => {})
+test('renders button', () => {
+  const wrapper = shallow(<App />)
+  const counterDisplay = wrapper.find('[data-test="counter-display"]')
 
-test('renders counter display', () => {})
+  expect(counterDisplay).toHaveLength(1)
+})
+
+test('renders counter display', () => {
+  const wrapper = shallow(<App />)
+  const incrementButton = wrapper.find('[data-test="increment-button"]')
+
+  expect(incrementButton).toHaveLength(1)
+})
 
 test('counter starts at 0', () => {})
 
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div data-test="component-app">Hola mundo</div>
+  return (
+    <div data-test="component-app">
+      <h1 data-test="counter-display">This counter is current</h1>
+      <button data-test="increment-button">Increment</button>
+    </div>
+  )
 }
 
 export default App
